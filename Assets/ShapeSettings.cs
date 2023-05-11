ef39bf782d36f395e09d15dc9dcc1253f0679a04
Benedetto Scala <100568040+benedettoscala@users.noreply.github.com>
2023-05-11 19:15:24 +0200

    Added noise filters and mask to create mountains

--- Assets/ShapeSettings.cs
+++ Assets/ShapeSettings.cs
@@ -6,4 +6,12 @@ using UnityEngine;
 public class ShapeSettings : ScriptableObject
 {
     public float planetRadius;
+    public NoiseLayer[] noiseLayers;
+    [System.Serializable]
+    public class NoiseLayer
+    {
+        public bool enabled = true;
+        public bool useFirstLayerAsMask;
+        public NoiseSettings noiseSettings;
+    }
 }
