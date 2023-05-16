52999ea9d6914d93beb7364416f69bf5661f5bbc
Benedetto Scala <100568040+benedettoscala@users.noreply.github.com>
2023-05-16 16:23:24 +0200

    Added Rigid Noise Filter

--- Assets/NoiseSettings.cs
+++ Assets/NoiseSettings.cs
@@ -5,12 +5,29 @@ using UnityEngine;
 [System.Serializable]
 public class NoiseSettings
 {
-    public float strength = 1;
-    public Vector3 centre;
-    [Range(1,8)]
-    public int numLayers = 1;
-    public float roughness = 2;
-    public float baseRoughness = 1;
-    public float persistence = .5f;
-    public float minValue;
+    public enum FilterType { Simple, Rigid};
+    public FilterType filterType;
+
+    [ConditionalHide("filterType", 0)]
+    public SimpleNoiseSettings simpleNoiseSettings;
+    [ConditionalHide("filterType", 1)]
+    public RigidNoiseSettings rigidNoiseSettings;
+
+    [System.Serializable]
+    public class SimpleNoiseSettings {
+        public float strength = 1;
+        public Vector3 centre;
+        [Range(1, 8)]
+        public int numLayers = 1;
+        public float roughness = 2;
+        public float baseRoughness = 1;
+        public float persistence = .5f;
+        public float minValue;
+    }
+
+    [System.Serializable]
+    public class RigidNoiseSettings : SimpleNoiseSettings
+    {
+        public float weightMultiplier = .8f;
+    }
 }
